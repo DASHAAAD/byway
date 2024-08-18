375fcccebd72d467b2773028ad92fba756e4ad2a
DASHAAAD <darya.farbak@yandex.ru>
2024-08-18 09:12:58 +0300

    контейнер и отступы стили

--- src/page/home/Home.tsx
+++ src/page/home/Home.tsx
@@ -18,115 +18,116 @@ import CategoryCard from '@/components/categoryCard/CategoryCard'
 const HomePage: FunctionComponent = () => {
     return (
         <div className={styles.wrapper}>
+
             <Header />
-            <main className={styles.main}>
+            <div className={styles.container}>
+                <main className={styles.main}>
 
-                <section className={`${styles.section} ${styles.advertisement}`}>
-                    <div className={styles.info}>
-                        <h1 className={styles.title}>Unlock Your Potential with Byway</h1>
-                        <p className={styles.text}>Welcome to Byway, where learning knows no bounds. We believe that education is the key to personal and professional growth, and we re here to guide you on your journey to success. </p>
-                        <button className={styles.button}>Start your instructor journey</button>
-                    </div>
-                    <Image src={people} alt={''} />
-                </section>
+                    <section className={`${styles.section} ${styles.advertisement}`}>
+                        <div className={styles.info}>
+                            <h1 className={styles.title}>Unlock Your Potential with Byway</h1>
+                            <p className={styles.text}>Welcome to Byway, where learning knows no bounds. We believe that education is the key to personal and professional growth, and we re here to guide you on your journey to success. </p>
+                            <button className={styles.button}>Start your instructor journey</button>
+                        </div>
+                        <Image src={people} alt={''} />
+                    </section>
 
-                <section className={styles.section}>
-                    <ul className={styles.List}>
-                        <li className={styles.Item}>
-                            <h3 className={styles.statisticsTitle}>250+</h3>
-                            <p className={styles.statisticsText}>Courses by our best mentors</p>
-                        </li>
-                        <li className={styles.Item}>
-                            <h3 className={styles.statisticsTitle}>1000+</h3>
-                            <p className={styles.statisticsText}>Courses by our best mentors</p>
-                        </li>
-                        <li className={styles.Item}>
-                            <h3 className={styles.statisticsTitle}>15+</h3>
-                            <p className={styles.statisticsText}>Courses by our best mentors</p>
-                        </li>
-                        <li className={styles.Item}>
-                            <h3 className={styles.statisticsTitle}>2400+</h3>
-                            <p className={styles.statisticsText}>Courses by our best mentors</p>
-                        </li>
-                    </ul>
-                </section>
+                    <section className={styles.section}>
+                        <ul className={styles.List}>
+                            <li className={styles.Item}>
+                                <h3 className={styles.statisticsTitle}>250+</h3>
+                                <p className={styles.statisticsText}>Courses by our best mentors</p>
+                            </li>
+                            <li className={styles.Item}>
+                                <h3 className={styles.statisticsTitle}>1000+</h3>
+                                <p className={styles.statisticsText}>Courses by our best mentors</p>
+                            </li>
+                            <li className={styles.Item}>
+                                <h3 className={styles.statisticsTitle}>15+</h3>
+                                <p className={styles.statisticsText}>Courses by our best mentors</p>
+                            </li>
+                            <li className={styles.Item}>
+                                <h3 className={styles.statisticsTitle}>2400+</h3>
+                                <p className={styles.statisticsText}>Courses by our best mentors</p>
+                            </li>
+                        </ul>
+                    </section>
 
-                <section className={styles.section}>
-                    <h2 className={styles.title}>Top Categories</h2>
-                    <ul className={styles.List}>
-                        <li className={styles.Item}>
-                            <AstrologyCards/>
-                        </li>
-                        <li className={styles.Item}>
-                            <AstrologyCards />
-                        </li>
-                        <li className={styles.Item}>
-                            <AstrologyCards />
-                        </li>
-                        <li className={styles.Item}>
-                            <AstrologyCards />
-                        </li>
-                    </ul>
-                </section>
+                    <section className={styles.section}>
+                        <h2 className={styles.title}>Top Categories</h2>
+                        <ul className={styles.List}>
+                            <li className={styles.Item}>
+                                <AstrologyCards />
+                            </li>
+                            <li className={styles.Item}>
+                                <AstrologyCards />
+                            </li>
+                            <li className={styles.Item}>
+                                <AstrologyCards />
+                            </li>
+                            <li className={styles.Item}>
+                                <AstrologyCards />
+                            </li>
+                        </ul>
+                    </section>
 
-                <section className={styles.section}>
-                    <h2 className={styles.title}>Top Courses</h2>
-                    <ul className={styles.List}>
-                        <li className={styles.Item}>
-                            <Card />
-                        </li>
-                        <li className={styles.Item}>
-                            <Card />
-                        </li>
-                        <li className={styles.Item}>
-                            <Card />
-                        </li>
-                        <li className={styles.Item}>
-                            <Card />
-                        </li>
-                    </ul>
-                </section>
+                    <section className={styles.section}>
+                        <h2 className={styles.title}>Top Courses</h2>
+                        <ul className={styles.List}>
+                            <li className={styles.Item}>
+                                <Card />
+                            </li>
+                            <li className={styles.Item}>
+                                <Card />
+                            </li>
+                            <li className={styles.Item}>
+                                <Card />
+                            </li>
+                            <li className={styles.Item}>
+                                <Card />
+                            </li>
+                        </ul>
+                    </section>
 
-                <section className={styles.section}>
-                    <h2 className={styles.title}>Top Instructors</h2>
-                    <ul className={styles.List}>
-                        <li className={styles.Item}>
-                            <CategoryCard />
-                        </li>
-                        <li className={styles.Item}>
-                            <CategoryCard />
-                        </li>
-                        <li className={styles.Item}>
-                            <CategoryCard />
-                        </li>
-                        <li className={styles.Item}>
-                            <CategoryCard />
-                        </li>
-                    </ul>
-                </section>
+                    <section className={styles.section}>
+                        <h2 className={styles.title}>Top Instructors</h2>
+                        <ul className={styles.List}>
+                            <li className={styles.Item}>
+                                <CategoryCard />
+                            </li>
+                            <li className={styles.Item}>
+                                <CategoryCard />
+                            </li>
+                            <li className={styles.Item}>
+                                <CategoryCard />
+                            </li>
+                            <li className={styles.Item}>
+                                <CategoryCard />
+                            </li>
+                        </ul>
+                    </section>
 
+                    <section className={`${styles.section} ${styles.advertisement}`}>
+                        <Image src={man} alt={''} />
+                        <div className={styles.info}>
+                            <h1 className={styles.title}>Become an Instructor</h1>
+                            <p className={styles.text}>Instructors from around the world teach millions of students on Byway. We provide the tools and skills to teach what you love.</p>
+                            <button className={styles.button}>Start your instructor journey</button>
+                        </div>
+                    </section>
 
-                <section className={`${styles.section} ${styles.advertisement}`}>
-                    <Image src={man} alt={''} />
-                    <div className={styles.info}>
-                        <h1 className={styles.title}>Become an Instructor</h1>
-                        <p className={styles.text}>Instructors from around the world teach millions of students on Byway. We provide the tools and skills to teach what you love.</p>
-                        <button className={styles.button}>Start your instructor journey</button>
-                    </div>
-                </section>
+                    <section className={`${styles.section} ${styles.advertisement}`}>
+                        <div className={styles.info}>
+                            <h1 className={styles.title}>Transform your life through education</h1>
+                            <p className={styles.text}>Learners around the world are launching new careers, advancing in their fields, and enriching their lives.</p>
+                            <button className={styles.button}>Checkout courses</button>
+                        </div>
+                        <Image src={manTwo} alt={''} />
+                    </section>
 
-                <section className={`${styles.section} ${styles.advertisement}`}>
-                    <div className={styles.info}>
-                        <h1 className={styles.title}>Transform your life through education</h1>
-                        <p className={styles.text}>Learners around the world are launching new careers, advancing in their fields, and enriching their lives.</p>
-                        <button className={styles.button}>Checkout courses</button>
-                    </div>
-                    <Image src={manTwo} alt={''} />
-                </section>
-
-
-            </main>
-
+                </main>
+            </div>
+            
             <Footer />
         </div>
 
--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -5,33 +5,35 @@ import styles from './footer.module.css'
 const Footer = () => {
     return (
         <div className={styles.footer}>
-            <div>
-                <div className={styles.logo}>
-                    <Logo />
-                    <a className={styles.link} href="#">Byway</a>
+            <div className={styles.container}>
+                <div>
+                    <div className={styles.logo}>
+                        <Logo />
+                        <a className={styles.link} href="#">Byway</a>
+                    </div>
+                    <p className={styles.text}>Empowering learners through accessible and engaging online education.
+                        Byway is an online learning platform dedicated to providing high-quality, flexible, and affordable educational experiences. </p>
                 </div>
-                <p className={styles.text}>Empowering learners through accessible and engaging online education.
-                    Byway is an online learning platform dedicated to providing high-quality, flexible, and affordable educational experiences. </p>
-            </div>
-            <div className={styles.item}>
-                <h2 className={styles.title}>Get Help</h2>
-                <p>Contact Us</p>
-                <p>Latest Articles</p>
-                <p>FAQ</p>
-            </div>
-            <div className={styles.item}>
-                <h2 className={styles.title}>Programs</h2>
-                <p>Art & Design</p>
-                <p>Business</p>
-                <p>IT & Software</p>
-                <p>Languages</p>
-                <p>Programming</p>
-            </div>
-            <div className={`${styles.item} ${styles.contacts}`}>
-                <h2 className={styles.title}>Contact Us</h2>
-                <p>Address: Saint-Petersburg, Kronverkskaya st., building 7</p>
-                <a href="tel:88005553535">Tel: 8(800) 555-35-35</a>
-                <a href="mailto:bywayedu@webkul.in">Mail: bywayedu@webkul.in</a>
+                <div className={styles.item}>
+                    <h2 className={styles.title}>Get Help</h2>
+                    <p>Contact Us</p>
+                    <p>Latest Articles</p>
+                    <p>FAQ</p>
+                </div>
+                <div className={styles.item}>
+                    <h2 className={styles.title}>Programs</h2>
+                    <p>Art & Design</p>
+                    <p>Business</p>
+                    <p>IT & Software</p>
+                    <p>Languages</p>
+                    <p>Programming</p>
+                </div>
+                <div className={`${styles.item} ${styles.contacts}`}>
+                    <h2 className={styles.title}>Contact Us</h2>
+                    <p>Address: Saint-Petersburg, Kronverkskaya st., building 7</p>
+                    <a href="tel:88005553535">Tel: 8(800) 555-35-35</a>
+                    <a href="mailto:bywayedu@webkul.in">Mail: bywayedu@webkul.in</a>
+                </div>
             </div>
         </div>
     )
