d6f19621a3ff642f22088ad5e7f77917511af0ce
DASHAAAD <darya.farbak@yandex.ru>
2024-08-21 01:11:34 +0300

    auth-register

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Form from "@/components/auth-loginForm/Form";
+import Register from "@/components/auth-register/Register";
 import HomePage from "@/page/home/Home";
 
 export default function Home() {
   return (
     <main className="bg-slate-500 flex flex-col h-screen justify-between">
-      <Form/>
+     <Register/>
     </main>
   );
 }
---
+++ src/components/auth-register/Register.tsx
@@ -0,0 +1,32 @@
+import ButtonBlack from '@/components/ButtonBlack/buttonBlack'
+import FormItem from '@/components/formItem/FormItem'
+import { Title } from '@/components/Title/Title'
+import Footer from '@/sections/Footer/Footer'
+import Header from '@/sections/Header/Header'
+import React, { FunctionComponent } from 'react'
+import girl from '../../image/girl2.png'
+import Image from 'next/image'
+import styles from '../../components/auth-register/Register.module.css'
+
+const Register: FunctionComponent = () => {
+    return (
+        <>
+            <Header />
+            <div className={styles.container}>
+                <Image src={girl} alt={''} />
+                <div className={styles.wrapper}>
+                    <Title text='Create Your Account' />
+                    <form action="">
+                        <FormItem title='Name' text='Name' />
+                        <FormItem title='Email' text='Email ID' />
+                        <FormItem title='Password' text='Enter Password' />
+                    </form>
+                    <ButtonBlack text='Create account' />
+                </div>
+            </div>
+            <Footer />
+        </>
+    )
+}
+
+export default Register
