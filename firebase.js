8ae99c915b20fac37be4ef08590d3d058137e530
DASHAAAD <darya.farbak@yandex.ru>
2024-09-04 20:38:28 +0300

    добавила firebaceConfig

--- firebase.js
+++ firebase.js
@@ -14,6 +14,22 @@ export const firebaseConfig = {
   appId: "1:1083403399112:web:8ca198ec8b4a1af50d1afd"
 };
 
+// mport { initializeApp } from 'firebase/app'
+// import { getAuth } from 'firebase/auth'
+
+const FIREBASE_CONFIG = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  appId: process.env.REACT_APP_FIREBASE_ID,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+}
+
+const firebaseApp = initializeApp(FIREBASE_CONFIG)
+
+// export const auth = getAuth(firebaseApp)
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
 const auth = getAuth(app)
--- src/page/auth-loginForm/Form.tsx
+++ src/page/auth-loginForm/Form.tsx
@@ -14,30 +14,21 @@ import ButtonBlack from '@/components/ButtonBlack/ButtonBlack'
 import { auth } from '../../../firebase'
 import { createUserWithEmailAndPassword } from 'firebase/auth'
 
+
 const Form: FunctionComponent = () => {
+
+
 
 
   const [email, setEmail] = useState('email')
   const [password, setPassword] = useState('password')
   const [error, setError] = useState('')
 
+
   const width = {
     width: '500px'
   }
 
-  
-
-  const requster: React.ChangeEventHandler<HTMLFormElement> = (e) => {
-    e.preventDefault()
-    setError('error')
-
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((user) => {
-        console.log(user)
-        setEmail('')
-        setPassword('')
-      })
-  }
 
   return (
     <>
@@ -45,7 +36,9 @@ const Form: FunctionComponent = () => {
       <div className={styles.container}>
         <div className={styles.wrapper}>
           <Title text='Log in to your account' />
-          <form className={styles.form} onSubmit={requster} action="">
+          <form className={styles.form} 
+          // onSubmit={requster} 
+          action="">
             <h2 className={styles.title}>Email</h2>
             <Input onChange={(e) => setEmail(e.currentTarget.value)} placeholder='email' />
             <h2 className={styles.title}>Password</h2>
