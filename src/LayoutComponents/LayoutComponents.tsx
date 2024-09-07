1d67a32af75e6ccfccc3b7f347340ccef0ed5f5e
DASHAAAD <darya.farbak@yandex.ru>
2024-09-07 21:28:59 +0300

    LayoutComponents

--- src/LayoutComponents/LayoutComponents.tsx
+++ src/LayoutComponents/LayoutComponents.tsx
@@ -7,9 +7,6 @@ import ImageItem from './components/Image'
 
 import { clsx } from 'clsx';
 
-
-
-
 const LayoutComponents: FunctionComponent = (props) => {
     return (
         <div>
@@ -19,7 +16,7 @@ const LayoutComponents: FunctionComponent = (props) => {
             <Title
                 title='Да, по новым стандартам HTML семантики их может быть несколько на странице'
                 className={styles.title} />
-            <section className={styles.twoColumn}>
+            <section className={clsx(styles.twoColumn, styles.section)}>
                 <Item
                     title={'Это - шаблон секции из двух колонок'}
                     text={'Колонки просто занимают всё свободное пространство и делят пополам'}
@@ -34,7 +31,7 @@ const LayoutComponents: FunctionComponent = (props) => {
                     classNameText={styles.textItem}
                     classNameWrapper={styles.wrapperItem} />
             </section>
-            <section className={styles.twoColumn}>
+            <section className={clsx(styles.twoColumn, styles.section)}>
                 <Item
                     title={'Но также колонкой может быть и фотография'}
                     text={'Как в примере справа'}
@@ -43,6 +40,124 @@ const LayoutComponents: FunctionComponent = (props) => {
                     classNameWrapper={clsx(styles.wrapperItem, styles.height)}
                 />
                 <ImageItem />
+            </section>
+            <section className={styles.section}>
+                <Item
+                    title={'Это - шаблон секции из одной колонки'}
+                    text={'Аналогично, заголовок второго уровня и контент'}
+                    classNameTitle={styles.titleItem}
+                    classNameText={styles.textItem}
+                    classNameWrapper={clsx(styles.wrapperItem, styles.width)}
+                />
+            </section>
+            <section className={clsx(styles.grid, styles.section)}>
+                <Item
+                    title={'Ещё есть вариант с неравными колонками'}
+                    text={'Одна меньше другой ровно в два раза'}
+                    classNameTitle={styles.titleItem}
+                    classNameText={styles.textItem}
+                    classNameWrapper={styles.wrapperItem}
+                />
+                <Item
+                    title={'Большая колонка'}
+                    text={'Растёт до бесконечности'}
+                    classNameTitle={styles.titleItem}
+                    classNameText={styles.textItem}
+                    classNameWrapper={clsx(styles.wrapperItem, styles.width)}
+                />
+            </section>
+            <section className={styles.section}>
+                <Title title={'Эта секция скроллится горизонтально'} className={styles.title} />
+                <div className={styles.horizontalScrolling}>
+                    <Item
+                        title={'Пример карточки'}
+                        text={'В этой секции могут быть плюс-минус любые компоненты, использование конкретного набора не подразумевается'}
+                        classNameTitle={styles.titleItem}
+                        classNameText={styles.textItem}
+                        classNameWrapper={clsx(styles.wrapperItem, styles.widthHeight)}
+                    />
+                    <Item
+                        title={'Пример карточки'}
+                        text={'Компоненты должны быть фиксированного размера, поскольку они не смогут адаптироваться под бесконечный контейнер'}
+                        classNameTitle={styles.titleItem}
+                        classNameText={styles.textItem}
+                        classNameWrapper={clsx(styles.wrapperItem, styles.widthHeight)}
+                    />
+                    <Item
+                        title={'Пример карточки'}
+                        text={'I was initially apprehensive, having no prior design experience. But the instructor, John Doe, did an amazing job of breaking down complex concepts into easily digestible modules. The video lectures were engaging, and the real-world examples really helped solidify my understanding.'}
+                        classNameTitle={styles.titleItem}
+                        classNameText={styles.textItem}
+                        classNameWrapper={clsx(styles.wrapperItem, styles.widthHeight)}
+                    />
+                    <Item
+                        title={'Пример карточки'}
+                        text={'I was initially apprehensive, having no prior design experience. But the instructor, John Doe, did an amazing job of breaking down complex concepts into easily digestible modules. The video lectures were engaging, and the real-world examples really helped solidify my understanding.'}
+                        classNameTitle={styles.titleItem}
+                        classNameText={styles.textItem}
+                        classNameWrapper={clsx(styles.wrapperItem, styles.widthHeight)}
+                    />
+                </div>
+            </section>
+            <section className={styles.section}>
+                <Title title={'Эта секция переносит карточки'} className={styles.title} />
+                <div className={styles.gridColumn}>
+                    <Item
+                        title={'Пример карточки'}
+                        text={'I was initially apprehensive, having no prior design experience. But the instructor, John Doe, did an amazing job of breaking down complex concepts into easily digestible modules. The video lectures were engaging, and the real-world examples really helped solidify my understanding.'}
+                        classNameTitle={styles.titleItem}
+                        classNameText={styles.textItem}
+                        classNameWrapper={clsx(styles.wrapperItem, styles.widthHeight)}
+                    />
+                    <Item
+                        title={'Пример карточки'}
+                        text={'I was initially apprehensive, having no prior design experience. But the instructor, John Doe, did an amazing job of breaking down complex concepts into easily digestible modules. The video lectures were engaging, and the real-world examples really helped solidify my understanding.'}
+                        classNameTitle={styles.titleItem}
+                        classNameText={styles.textItem}
+                        classNameWrapper={clsx(styles.wrapperItem, styles.widthHeight)}
+                    />
+                    <Item
+                        title={'Пример карточки'}
+                        text={'I was initially apprehensive, having no prior design experience. But the instructor, John Doe, did an amazing job of breaking down complex concepts into easily digestible modules. The video lectures were engaging, and the real-world examples really helped solidify my understanding.'}
+                        classNameTitle={styles.titleItem}
+                        classNameText={styles.textItem}
+                        classNameWrapper={clsx(styles.wrapperItem, styles.widthHeight)}
+                    />
+                    <Item
+                        title={'Пример карточки'}
+                        text={'I was initially apprehensive, having no prior design experience. But the instructor, John Doe, did an amazing job of breaking down complex concepts into easily digestible modules. The video lectures were engaging, and the real-world examples really helped solidify my understanding.'}
+                        classNameTitle={styles.titleItem}
+                        classNameText={styles.textItem}
+                        classNameWrapper={clsx(styles.wrapperItem, styles.widthHeight)}
+                    />
+                    <Item
+                        title={'Пример карточки'}
+                        text={'I was initially apprehensive, having no prior design experience. But the instructor, John Doe, did an amazing job of breaking down complex concepts into easily digestible modules. The video lectures were engaging, and the real-world examples really helped solidify my understanding.'}
+                        classNameTitle={styles.titleItem}
+                        classNameText={styles.textItem}
+                        classNameWrapper={clsx(styles.wrapperItem, styles.widthHeight)}
+                    />
+                    <Item
+                        title={'Пример карточки'}
+                        text={'I was initially apprehensive, having no prior design experience. But the instructor, John Doe, did an amazing job of breaking down complex concepts into easily digestible modules. The video lectures were engaging, and the real-world examples really helped solidify my understanding.'}
+                        classNameTitle={styles.titleItem}
+                        classNameText={styles.textItem}
+                        classNameWrapper={clsx(styles.wrapperItem, styles.widthHeight)}
+                    />
+                </div>
+            </section>
+            <section className={clsx(styles.section, styles.flex)}>
+                <Title title={'Рядом с заголовком можно поставить кнопку'} className={styles.title} />
+                <button className={styles.button}>Кнопка</button>
+            </section>
+            <section className={styles.section}>
+                <Item
+                    title={'Секция не обязательно должна быть залита цветом'}
+                    text={'И не обязательно должна иметь отступы В макете это для видимости, для примера'}
+                    classNameTitle={styles.titleItem}
+                    classNameText={styles.textItem}
+                    classNameWrapper={clsx(styles.wrapperItem, styles.width)}
+                />
             </section>
         </div>
     )
