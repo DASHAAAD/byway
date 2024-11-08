274b8034ff01ba15fabc53d0b166cb5a006b4c7a
DASHAAAD <darya.farbak@yandex.ru>
2024-11-09 00:37:54 +0300

    -

--- src/components/categoryCard/CategoryCard.stories.ts
+++ src/components/categoryCard/CategoryCard.stories.ts
@@ -1,9 +1,22 @@
-import type { StoryObj } from "@storybook/react";
+import type { Meta, StoryObj, Decorator } from "@storybook/react";
 import CategoryCard from "./CategoryCard";
+import style from "styled-jsx/style";
 
-const meta = {
-  title: "default/CategoryCard",
+// const meta = {
+//   title: "default/CategoryCard",
+//   component: CategoryCard,
+//   decorators: [
+//     (Story) => (
+//       <div style={{ margin: '3em' }}>
+//        jjj
+//         <Story />
+//       </div>
+//     ),
+// };
+
+const meta: Meta<typeof CategoryCard> = {
   component: CategoryCard,
+  decorators: [(CategoryCard) => CategoryCard()],
 };
 
 export default meta;
