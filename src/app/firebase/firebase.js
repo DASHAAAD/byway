5c006b154358398f8c21486f36667f12f00d197f
DASHAAAD <darya.farbak@yandex.ru>
2024-11-15 03:10:57 +0300

    env файлы

--- src/app/firebase/firebase.js
+++ src/app/firebase/firebase.js
@@ -4,14 +4,26 @@ import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDAX6eEEh4_DUjBXfLhQAuBFRZmCnVO9aU",
+//   authDomain: "by-way-4a22c.firebaseapp.com",
+//   databaseURL: "https://by-way-4a22c-default-rtdb.firebaseio.com",
+//   projectId: "by-way-4a22c",
+//   storageBucket: "by-way-4a22c.firebasestorage.app",
+//   messagingSenderId: "1083403399112",
+//   appId: "1:1083403399112:web:8ca198ec8b4a1af50d1afd"
+// };
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDAX6eEEh4_DUjBXfLhQAuBFRZmCnVO9aU",
-  authDomain: "by-way-4a22c.firebaseapp.com",
-  databaseURL: "https://by-way-4a22c-default-rtdb.firebaseio.com",
-  projectId: "by-way-4a22c",
-  storageBucket: "by-way-4a22c.firebasestorage.app",
-  messagingSenderId: "1083403399112",
-  appId: "1:1083403399112:web:8ca198ec8b4a1af50d1afd"
+  apiKey: process.env.DB_APIKEY,
+  authDomain:  process.env.DB_AUTHDOMAIN,
+  databaseURL: process.env.DB_DATABESEURL,
+  projectId: process.env.DB_PROJECTID,
+  storageBucket: process.env.DB_STORAGEBUCKET,
+  messagingSenderId: process.env.DB_MESSAGINSENDERID,
+  appId: process.env.DB_APPID,
+  measurementId: process.env.DB_MEASUREMENTID
 };
 
 export const app = initializeApp(firebaseConfig);
